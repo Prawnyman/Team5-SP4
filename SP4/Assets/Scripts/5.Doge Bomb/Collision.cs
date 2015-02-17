09225564e980cadbad93b0c30faeac62557683e5
Charlene Chow <ayndrfydan@hotmail.com>
2015-02-17 18:01:06 +0800

    Updated Bomb Texture/ Tilt Controls
    no more looking like a fish :D

--- SP4/Assets/Scripts/5.Doge Bomb/Collision.cs
+++ SP4/Assets/Scripts/5.Doge Bomb/Collision.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Collision : MonoBehaviour {
 
+	int health = 3;
 	void OnCollisionEnter2D (Collision2D col)
 	{
 		Debug.Log("collision name = " + col.gameObject.name);
@@ -10,7 +11,21 @@ public class Collision : MonoBehaviour {
 		if ((col.gameObject.name == "BombTemp(Clone)")||(col.gameObject.name == "BombTemp"))
 		{
 			Destroy(col.gameObject);
+			health=health -1;
+
+			if (health <=0)
+			{
+				iflose ();
+			}
 			// lose condition (get hit by bomb)
 		}
+	}
+
+
+	void iflose ()
+	{
+
+		Debug.Log("Lose!!!!");
+		Destroy(gameObject);
 	}
 }
