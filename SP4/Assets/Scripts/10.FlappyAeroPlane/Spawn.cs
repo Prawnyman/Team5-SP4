accdf5f7edab2c646411297120bd818aecc3f78b
SunShuyi <prettycure_16@hotmail.com>
2015-02-25 09:37:40 +0800

    FlappyPlane(with collisions)

--- SP4/Assets/Scripts/10.FlappyAeroPlane/Spawn.cs
+++ SP4/Assets/Scripts/10.FlappyAeroPlane/Spawn.cs
@@ -1,15 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Spawn : MonoBehaviour {
+public class Spawn : MonoBehaviour 
+{
 
+	public GameObject Obstacles;
+	int Score = 0;
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		InvokeRepeating ("CreateObstacles", 8.0f, 6.0f);
 	}
-	
+
+	void OnGui()
+	{
+//		GUI.color = Color.red;
+//		GUILayout.Label (" Score: " + Score.ToString());
+	}
 	// Update is called once per frame
-	void Update () {
-	
+	void CreateObstacles()
+	{
+		Instantiate (Obstacles);
+		Score++;
+		Debug.Log (Score);
+	}
+	void Update ()
+	{
 	}
 }
