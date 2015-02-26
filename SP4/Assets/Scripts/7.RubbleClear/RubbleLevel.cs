b474a9540a709e527b098219e6579da660cdc6a5
Charlene Chow <ayndrfydan@hotmail.com>
2015-02-26 17:04:12 +0800

    update Rubble game

--- SP4/Assets/Scripts/7.RubbleClear/RubbleLevel.cs
+++ SP4/Assets/Scripts/7.RubbleClear/RubbleLevel.cs
@@ -4,22 +4,40 @@ using System.Collections;
 public class RubbleLevel : MonoBehaviour {
 	public GameObject Rock;
 	public GameObject Picture;
+	public GameObject Picture2;
 	// Use this for initialization
 	void Start () {
 		int z = 0;
-		Instantiate (Picture);
-		for (int i =0; i<5; i++)
+		float pic = Random.Range (1, 10);
+		if (pic < 5) {
+			Instantiate (Picture);
+		} else {
+			Instantiate (Picture2);
+		}
+
+
+		for (int i =0; i<3; i++)
 		{
 			float x = Random.Range (-7, 0);
-			float y =Random.Range (0, 4);
-			
+			float y =Random.Range (-4, 0);
 			Instantiate (Rock, new Vector3 (x, y, z), Quaternion.identity);
 			z++;
-			 x = Random.Range (0, 7);
-			 y =Random.Range (-4, 0);
-			
+
+			 x = Random.Range (-7, 0);
+			 y =Random.Range (0, 4);
 			Instantiate (Rock, new Vector3 (x, y, z), Quaternion.identity);
 			z++;
+
+			x = Random.Range (0, 7);
+			y =Random.Range (-4, 0);
+			Instantiate (Rock, new Vector3 (x, y, z), Quaternion.identity);
+			z++;
+
+			x = Random.Range (0, 7);
+			y =Random.Range (0, 4);
+			Instantiate (Rock, new Vector3 (x, y, z), Quaternion.identity);
+			z++;
+
 
 		}
 	
